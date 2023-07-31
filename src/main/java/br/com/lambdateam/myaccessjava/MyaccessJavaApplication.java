1429bac61a15109c8b5dd920f1ce064cb44e5034
Kleber Barreto de Macedo <kleber.barreto@msn.com>
2023-07-31 20:27:15 -0300

    Adicionados headers de segurança

--- src/main/java/br/com/lambdateam/myaccessjava/MyaccessJavaApplication.java
+++ src/main/java/br/com/lambdateam/myaccessjava/MyaccessJavaApplication.java
@@ -4,10 +4,12 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@ServletComponentScan
 public class MyaccessJavaApplication {
 
 	public static void main(String[] args) {
